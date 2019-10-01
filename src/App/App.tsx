ae847f6fc7289ca805dc398c42103e69a996c805
Saul Solano <saul3d@yahoo.com>
2019-09-30 23:52:02 -0500

    added navbar to project

---
+++ src/App/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import MyNavbar from '../components/MyNavbar/MyNavbar';
+import './App.scss';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App: React.FC = () => {
+  return (
+    
+    <div>
+      <MyNavbar />
+    </div>
+
+  );
+}
+
+export default App;
